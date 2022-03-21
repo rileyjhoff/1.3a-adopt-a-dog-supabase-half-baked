6ebfbce4a83061c94d2c3954211260abcad9c6b2
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-21 12:45:27 -0700

    wrote event listener to fetch all dogs and render and append all dog cards to home page

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-import { getDogs, getDog } from './fetch-utils.js';
+import { getDogs } from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
-getDogs();
-getDog(1);
-getDog(2);
-getDog(3);
-getDog(4);
-getDog(5);
-getDog(6);
-
 
 // on load
-// fetch all dogs
-// render and append all dog cards to the container
+window.addEventListener('load', async() => {
+    // fetch all dogs
+    const dogs = await getDogs();
+    // render and append all dog cards to the container
+    for (let dog of dogs) {
+        const dogCard = renderDogCard(dog);
+
+        dogListContainer.append(dogCard);
+    }
+});
