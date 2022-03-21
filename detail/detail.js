e7d3e7a712f9ff513adc8fa1b30fcd4ffa79657e
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-21 14:23:28 -0700

    change detail page title to dog's name once data is fetched from supabase

--- detail/detail.js
+++ detail/detail.js
@@ -2,6 +2,7 @@ import { getDog } from '../fetch-utils.js';
 import { renderDogDetail } from '../render-utils.js';
 
 const dogDetailContainer = document.getElementById('dog-detail-container');
+const dogName = document.getElementById('dog-name');
 
 // on load
 window.addEventListener('load', async() => {
@@ -10,7 +11,10 @@ window.addEventListener('load', async() => {
     const id = data.get('id');
     // use the id to fetch the dog
     const dog = await getDog(id);
+    // changing page title to the dog's name
+    dogName.textContent = `${dog.name}`;
     // render and append this dog's details to the container
     const dogDetail = renderDogDetail(dog);
+
     dogDetailContainer.append(dogDetail);
 });
