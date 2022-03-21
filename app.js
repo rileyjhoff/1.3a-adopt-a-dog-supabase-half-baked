74989fb2d235c58a3099c2012a067d4198d99529
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-21 12:38:09 -0700

    wrote fetch functions for getDogs and getDog

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-import { getDogs } from './fetch-utils.js';
+import { getDogs, getDog } from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
+
+getDogs();
+getDog(1);
+getDog(2);
+getDog(3);
+getDog(4);
+getDog(5);
+getDog(6);
+
 
 // on load
 // fetch all dogs
