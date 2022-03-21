72cab519444ca3fbff3c87754fd6ce6a4612a4a6
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-21 13:00:34 -0700

    wrote event listener to fetch a single dog and render and append dog details to detail page

--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,13 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
-// get the id from URL
-// use the id to fetch the dog
-// render and append this dog's details to the container
+window.addEventListener('load', async() => {
+    // get the id from URL
+    const data = new URLSearchParams(window.location.search);
+    const id = data.get('id');
+    // use the id to fetch the dog
+    const dog = await getDog(id);
+    // render and append this dog's details to the container
+    const dogDetail = renderDogDetail(dog);
+    dogDetailContainer.append(dogDetail);
+});
